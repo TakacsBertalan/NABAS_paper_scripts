2a2091976b39053911ed03d573e1c12849ea4a27
Erda Alexandria Qorri <97244940+erdaqorri@users.noreply.github.com>
2022-12-06 13:04:07 +0100

    Add files via upload

---
+++ Zymo_low_seq.R
@@ -0,0 +1,63 @@
+# R script to generate the stacked barcharts for the Zymo dataset in the paper: "Accurate alignment-based classification of metagenomic sequences with NABAS+"
+# Author: Erda Qorri
+# Date: 06-12-2022
+
+# Load libraries
+library(ggplot2)
+library(tidyverse)
+library(dplyr)
+library(ggtext)
+library(ggbreak)
+library(pals)
+library(ggpubr)
+library(patchwork)
+library(magrittr)
+
+# Import dataset
+abundances_small <- read.csv("path_to_file.csv", sep = "\t", header = FALSE)
+
+# Generate plot for deeply sequenced samples
+abundances_small_plot <- abundances_small %>%
+  mutate(Species = factor(Species, levels = c("Listeria monocytogenes",
+                                              "Pseudomonas aeruginosa",
+                                              "Bacillus subtilis group*",
+                                              "Escherichia coli",	
+                                              "Salmonella enterica",
+                                              "Lactobacillus fermentum group*",
+                                              "Enterococcus faecalis",
+                                              "Staphylococcus aureus",
+                                              "Misidentified"))) %>%
+  ggplot(., aes(x = Software, y = `Relative abundance`, fill = Species)) +
+  geom_bar(stat="identity", width = 0.4) +
+  scale_y_continuous(limits = c(0,100), expand = c(0, 0)) +
+  theme_classic() +
+  xlab("") +
+  ylab("Relative Abundance (%)") +
+  theme(axis.text.x = element_text(hjust = 1,
+                                   size = 12,
+                                   angle = 30,
+                                   vjust = 1,
+                                   color = "black"),
+        axis.text.y = element_text(hjust = 0.8,
+                                   size = 11,
+                                   angle = 0,
+                                   color = "black"),
+        axis.title.y = element_text(hjust = 0.5,
+                                    size = 13,
+                                    vjust = 1,
+                                    color = "black"),
+        legend.position =  "right",
+        legend.text.align = 0) +
+  scale_fill_manual(values=as.vector(tol(26)),
+                    "Species",
+                    labels = c(expression(italic("Listeria monocytogenes")), 
+                               expression(italic("Pseudomonas aeruginosa")),
+                               expression(paste(italic('Bacillus subtilis'), " group*")),
+                               expression(italic("Escherichia coli")),
+                               expression(italic("Salmonella enterica")),
+                               expression(paste(italic('Lactobacillus fermentum'),  " group*")),
+                               expression(italic("Enterococcus faecalis")),
+                               expression(italic("Staphylococcus aureus")),
+                               expression(plain("Misidentified")))) 
+
+print(abundances_small_plot)
