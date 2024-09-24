8c7d38cc48cbf2e4833f798ac24f8453a83551a1
Bertalan Takács <47786829+TakacsBertalan@users.noreply.github.com>
2024-09-24 09:45:14 +0200

    Update SimpleFileWriter.java

--- NABASCreateDatabase/src/SimpleFileWriter.java
+++ NABASCreateDatabase/src/SimpleFileWriter.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
-/**
- *
- * @author deltagene
- */
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
