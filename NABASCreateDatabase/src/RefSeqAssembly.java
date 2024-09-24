a6f804152b3f5b6060cba0e5524db58d3ecb8125
Bertalan Takács <47786829+TakacsBertalan@users.noreply.github.com>
2024-09-24 09:46:10 +0200

    Update RefSeqAssembly.java

--- NABASCreateDatabase/src/RefSeqAssembly.java
+++ NABASCreateDatabase/src/RefSeqAssembly.java
@@ -3,16 +3,9 @@ import java.io.File;
 import java.time.LocalDate;
 import java.util.Date;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
- * @author deltagene
+ * @author Takács Bertalan
  */
 public class RefSeqAssembly {
 
