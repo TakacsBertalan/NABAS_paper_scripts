71c00cbf9d14ad9537883c3a575c4373329f91a6
Bertalan Takács <47786829+TakacsBertalan@users.noreply.github.com>
2024-09-24 09:45:26 +0200

    Update SimpleFileReader.java

--- NABASCreateDatabase/src/SimpleFileReader.java
+++ NABASCreateDatabase/src/SimpleFileReader.java
@@ -1,10 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
