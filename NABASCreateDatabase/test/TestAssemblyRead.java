62353c346f6c38e1a2516740a41d0e70116e5490
Bertalan Takács <47786829+TakacsBertalan@users.noreply.github.com>
2024-09-24 09:44:49 +0200

    Update TestAssemblyRead.java

--- NABASCreateDatabase/test/TestAssemblyRead.java
+++ NABASCreateDatabase/test/TestAssemblyRead.java
@@ -9,15 +9,11 @@ import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
- * @author deltagene
+ * @author Takács Bertalan
  */
+
 public class TestAssemblyRead {
 
     public static void main(String[] args) {
