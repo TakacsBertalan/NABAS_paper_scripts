dfdfc7eacd204fa0c80a983f2bb6b73d673912dd
Bertalan Takács <47786829+TakacsBertalan@users.noreply.github.com>
2024-09-25 14:31:12 +0200

    Update Species.java

--- NABASCompare/Species.java
+++ NABASCompare/Species.java
@@ -1,14 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package hu.deltabio.nabas.compare;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author deltagene
+ * @author Takács Bertalan
  */
 public class Species {
     String kingdom;
